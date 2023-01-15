4795dc9d297fb8d8d1716b77a87f98966af5b172
Nomsku <albin.sihvola@metropolia.fi>
2023-01-15 23:09:07 +0200

    fix

--- src/numgam.js
+++ src/numgam.js
@@ -1,7 +1,7 @@
 "use strict";
         
 const timeText = document.querySelector("#timer");
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() (max - min + 1) + min);
 
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
